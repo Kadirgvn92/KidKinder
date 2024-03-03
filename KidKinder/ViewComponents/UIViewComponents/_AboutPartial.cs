d7c38202a21c0213820981fcd41e066e7c9c3505
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-03 13:30:11 +0300

    PartialViews designed

---
+++ KidKinder/ViewComponents/UIViewComponents/_AboutPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.ViewComponents.UIViewComponents;
+
+public class _AboutPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
