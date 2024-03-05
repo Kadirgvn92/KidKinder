6678e7c4a5a4d37afc6bcf1956141651886ad588
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-05 13:59:22 +0300

    Dashboard created and started to design

---
+++ KidKinder/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using KidKinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.Controllers;
+public class DashboardController : Controller
+{
+    private readonly Context _context;
+
+    public DashboardController(Context context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+}
