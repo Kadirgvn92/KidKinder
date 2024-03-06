c0797bb6bbcda0aa0a7a8125115a53ae45d6a247
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-06 17:45:06 +0300

    Teacher section completed

--- KidKinder/Areas/Admin/Controllers/TeacherController.cs
+++ KidKinder/Areas/Admin/Controllers/TeacherController.cs
@@ -1,5 +1,9 @@
-﻿using KidKinder.Entities;
+﻿using KidKinder.Areas.Admin.Models;
+using KidKinder.Entities;
+using KidKinder.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace KidKinder.Controllers;
 
@@ -16,7 +20,104 @@ public class TeacherController : Controller
 
     public IActionResult Index()
     {
-        var values = _context.Teachers.ToList();
+        var values = _context.Teachers.Include(x => x.Class).ToList();
+       
         return View(values);
+    }
+    public IActionResult DeleteClass(int id)
+    {
+        var values = _context.Teachers.FirstOrDefault(x => x.TeacherID == id);
+        _context.Teachers.Remove(values);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult CreateTeacher()
+    {
+
+        List<SelectListItem> classes = (from x in _context.KidClasses.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.Name,
+                                            Value = x.KidClassID.ToString()
+                                        }).ToList();
+        ViewBag.v = classes;
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateTeacher(CreateTeacherViewModel model)
+    {
+        var resource = Directory.GetCurrentDirectory();
+        var extension = Path.GetExtension(model.Image.FileName);
+        var imageName = Guid.NewGuid() + extension;
+        var savelocation = resource + "/wwwroot/teacherImages/" + imageName;
+        var stream = new FileStream(savelocation, FileMode.Create);
+        await model.Image.CopyToAsync(stream);
+
+        Teacher teacher = new Teacher
+        {
+           Name = model.Name,
+           Branch = model.Branch,
+           FacebookUrl = model.FacebookUrl,
+           InstagramUrl = model.InstagramUrl,
+           LinkedinUrl = model.LinkedinUrl,
+           ImageUrl = imageName,
+           KidClassID = model.KidClassID
+        };
+
+        _context.Teachers.Add(teacher);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult UpdateTeacher(int id)
+    {
+        var values = _context.Teachers.FirstOrDefault(x => x.TeacherID == id);
+
+        if (values != null)
+        {
+            UpdateTeacherViewModel model = new UpdateTeacherViewModel
+            {
+                TeacherID = values.TeacherID,
+                Branch = values.Branch,
+                ImageUrl= values.ImageUrl,
+                LinkedinUrl = values.LinkedinUrl,   
+                InstagramUrl= values.InstagramUrl,
+                FacebookUrl= values.FacebookUrl,
+                Name = values.Name  
+            };
+
+            return View(model);
+        }
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> UpdateTeacher(UpdateTeacherViewModel model)
+    {
+        var values = _context.Teachers.FirstOrDefault(x => x.TeacherID == model.TeacherID);
+
+        if (model.Image != null)
+        {
+            var resource = Directory.GetCurrentDirectory();
+            var extension = Path.GetExtension(model.Image.FileName);
+            var imageName = Guid.NewGuid() + extension;
+            var savelocation = resource + "/wwwroot/teacherImages/" + imageName;
+            var stream = new FileStream(savelocation, FileMode.Create);
+            await model.Image.CopyToAsync(stream);
+            model.ImageUrl = imageName;
+        }
+
+
+        values.Name = model.Name;
+        values.Branch = model.Branch;
+        values.ImageUrl = model.ImageUrl;
+        values.LinkedinUrl = model.LinkedinUrl;
+        values.InstagramUrl = model.InstagramUrl;
+        values.FacebookUrl = model.FacebookUrl;
+
+        _context.Update(values);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
---
+++ KidKinder/Areas/Admin/Models/CreateTeacherViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KidKinder.Areas.Admin.Models;
+
+public class CreateTeacherViewModel
+{
+    public string Name { get; set; }
+    public string Branch { get; set; }
+    public string ImageUrl { get; set; }
+    public string FacebookUrl { get; set; }
+    public string InstagramUrl { get; set; }
+    public string LinkedinUrl { get; set; }
+    public int KidClassID { get; set; }
+    public IFormFile Image { get; set; }
+}
