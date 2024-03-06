486b0894a0a93cb9aaf18fa85570f49667c129cd
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-06 09:36:11 +0300

    Dashboard strengthen with progressbar on Class

---
+++ KidKinder/Models/ProgressBarViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace KidKinder.Models;
+
+public class ProgressBarViewModel
+{
+    public string ClassName { get; set; }
+    public double Capacity { get; set; }
+}
---
+++ KidKinder/ViewComponents/AdminViewComponents/_AdminProgressPartial.cs
@@ -0,0 +1,24 @@
+﻿using KidKinder.Entities;
+using KidKinder.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.ViewComponents.AdminViewComponents;
+
+public class _AdminProgressPartial : ViewComponent
+{
+    private readonly Context _context;
+
+    public _AdminProgressPartial(Context context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var capacity = _context.KidClasses.Select(x => new ProgressBarViewModel{
+            ClassName = x.Name,
+            Capacity = ((double)x.Kids.Count / x.TotalSeats) * 100 
+        }).ToList();
+        return View(capacity);  
+    }
+}
