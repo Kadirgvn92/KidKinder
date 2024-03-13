96bc14f112f3e99501e8ff9f8994e685d6905741
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-13 12:19:39 +0300

    Kids section completed

--- KidKinder/Areas/Admin/Controllers/KidController.cs
+++ KidKinder/Areas/Admin/Controllers/KidController.cs
@@ -2,6 +2,7 @@
 using KidKinder.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace KidKinder.Areas.Admin.Controllers;
 
@@ -18,7 +19,7 @@ public class KidController : Controller
 
     public IActionResult Index()
     {
-        var values = _context.Kids.ToList();
+        var values = _context.Kids.Include(x => x.Parents).Include(y => y.Class).ToList();
         return View(values);
     }
     public IActionResult DeleteKid(int id)
@@ -37,18 +38,18 @@ public class KidController : Controller
                                             Text = x.Name,
                                             Value = x.ParentID.ToString()
                                         }).ToList();
-        ViewBag.v = parents;
+        ViewBag.v1 = parents;
         List<SelectListItem> classes = (from x in _context.KidClasses.ToList()
                                         select new SelectListItem
                                         {
                                             Text = x.Name,
                                             Value = x.KidClassID.ToString()
                                         }).ToList();
-        ViewBag.v = classes;
+        ViewBag.v2 = classes;
         return View();
     }
     [HttpPost]
-    public async Task<IActionResult> CreateKid(CreateKidViewModel model)
+    public IActionResult CreateKid(CreateKidViewModel model)
     {
 
         Kid Kid = new Kid
@@ -70,12 +71,30 @@ public class KidController : Controller
     {
         var values = _context.Kids.FirstOrDefault(x => x.KidID == id);
 
+        List<SelectListItem> parents = (from x in _context.Parents.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.Name,
+                                            Value = x.ParentID.ToString()
+                                        }).ToList();
+        ViewBag.v1 = parents;
+        List<SelectListItem> classes = (from x in _context.KidClasses.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.Name,
+                                            Value = x.KidClassID.ToString()
+                                        }).ToList();
+        ViewBag.v2 = classes;
+
         if (values != null)
         {
             UpdateKidViewModel model = new UpdateKidViewModel
             {
                 KidID = values.KidID,
-                Name = values.Name
+                Name = values.Name,
+                Age= values.Age,    
+                ParentID = values.ParentID,
+                KidClassID = values.KidClassID,
             };
 
             return View(model);
@@ -89,6 +108,9 @@ public class KidController : Controller
 
 
         values.Name = model.Name;
+        values.ParentID = model.ParentID;
+        values.KidClassID = model.KidClassID;
+        values.Age = model.Age;
 
         _context.Update(values);
         _context.SaveChanges();
