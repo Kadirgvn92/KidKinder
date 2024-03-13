f61ed23b413d30a8f1f23d8709a9c982124883e6
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-13 12:35:56 +0300

    Main Class section completed

---
+++ KidKinder/Controllers/ClassesController.cs
@@ -0,0 +1,19 @@
+﻿using KidKinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.Controllers;
+public class ClassesController : Controller
+{
+    private readonly Context _context;
+
+    public ClassesController(Context context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _context.KidClasses.ToList();
+        return View(values);
+    }
+}
