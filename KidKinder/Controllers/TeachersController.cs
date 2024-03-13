f2364250314f014b0a7ab3a16a09582b8459a71d
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-13 12:42:07 +0300

    Main Teachers section completed

---
+++ KidKinder/Controllers/TeachersController.cs
@@ -0,0 +1,19 @@
+﻿using KidKinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.Controllers;
+public class TeachersController : Controller
+{
+    private readonly Context _context;
+
+    public TeachersController(Context context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _context.Teachers.ToList();
+        return View(values);
+    }
+}
