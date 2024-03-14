18b033051edf613120bd81a6a4a26efa5a58e4fc
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-14 12:22:00 +0300

    Testimonial section completed

---
+++ KidKinder/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,22 @@
+﻿using KidKinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class TestimonialController : Controller
+{
+    private readonly Context _context;
+
+    public TestimonialController(Context context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _context.Testimonials.ToList();
+        return View(values);
+    }
+}
