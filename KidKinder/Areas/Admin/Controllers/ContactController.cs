3b29ca06de925f88bf948340c7f5e90dff736a6a
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-14 15:30:11 +0300

    Contact section completed

---
+++ KidKinder/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,56 @@
+﻿using KidKinder.Areas.Admin.Models;
+using KidKinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KidKinder.Areas.Admin.Controllers;
+[Area("Admin")]
+[Route("Admin/[controller]/[action]/{id?}")]
+public class ContactController : Controller
+{
+    private readonly Context _context;
+
+    public ContactController(Context context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var values = _context.GetInTouches.ToList();
+        return View(values);
+    }
+    [HttpGet]
+    public IActionResult UpdateContact(int id)
+    {
+        var values = _context.GetInTouches.FirstOrDefault(x => x.GetInTouchID == id);
+        if(values != null) 
+        {
+            UpdateGetInTouchViewModel model = new UpdateGetInTouchViewModel
+            {
+                GetInTouchID = values.GetInTouchID,
+                Address = values.Address,
+                Mail = values.Mail,
+                OpeningDays = values.OpeningDays,
+                Phone = values.Phone,
+                OpeningHours = values.OpeningHours
+            };
+            return View(model);
+        }
+        return View();
+    }
+    [HttpPost]
+    public IActionResult UpdateContact(UpdateGetInTouchViewModel model)
+    {
+        var values = _context.GetInTouches.FirstOrDefault(x => x.GetInTouchID == model.GetInTouchID);
+
+        values.Address = model.Address;
+        values.Mail = model.Mail;
+        values.OpeningDays = model.OpeningDays;
+        values.Phone = model.Phone;
+        values.OpeningHours = model.OpeningHours;
+
+        _context.Update(values);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
---
+++ KidKinder/Areas/Admin/Models/UpdateGetInTouchViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace KidKinder.Areas.Admin.Models;
+
+public class UpdateGetInTouchViewModel
+{
+
+    public int GetInTouchID { get; set; }
+    public string Address { get; set; }
+    public string Mail { get; set; }
+    public string Phone { get; set; }
+    public string OpeningHours { get; set; }
+    public string OpeningDays { get; set; }
+}
